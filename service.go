6a54519cca05229e02ff85ffd7ed161b011124a1
abwilson23 <andrewbwilson23@gmail.com>
2024-05-06 14:32:59 +0100

    initial commit

---
+++ service.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	e.Static("/public", "public")
+	e.GET("/", func(context echo.Context) error {
+		return context.String(http.StatusOK, "Hello world!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
